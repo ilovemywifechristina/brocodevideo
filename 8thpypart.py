949b34192a0db9482a6d9b3250107d20c3a6b97b
tinibooshusband <2008.elier.2008@gmail.com>
2025-11-27 21:18:55 -0700

    commit

--- 8thpypart.py
+++ 8thpypart.py
@@ -1,2 +1,14 @@
-print("Hello World")
-print("Hello World")
+#python weight converter
+weight = float(input("enter your weight here: "))
+unit = input("kilos or pounds? (K or L): ")
+
+if unit == "K":
+    weight = weight * 2.205
+    unit = "lbs"
+    print(f"your weight is: {round(weight, 1)} {unit}")
+elif unit == "L":
+    weight = weight / 2.205
+    unit = "kgs"
+    print(f"your weight is: {round(weight, 1)} {unit}")
+else:
+    print(f"{unit} was not valid")
